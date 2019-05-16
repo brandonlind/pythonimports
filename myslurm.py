97f1b559007b745342b6a7f8184dc5dd713e8792
Brandon Lind <lindb@vcu.edu>
2019-05-15 21:37:29 -0700

    first commit

---
+++ myslurm.py
@@ -0,0 +1,136 @@
+import os
+
+def get_mems(infos:dict, unit='MB') -> list:
+    mems = []
+    for key,info in infos.items():
+        if 'running' in info[3].lower() or 'pending' in info[3].lower():
+            continue
+        mem, units = info[-2].split()[-2:]
+        if units == 'GB':
+            mem = float(mem)*1024
+        elif units == 'EB':
+            pass
+        else:
+            try:
+                assert units == 'MB'
+            except AssertionError:
+                print('info = ', info)
+            mem = float(mem)
+        if unit == 'GB':
+            mem = mem/1024
+        mems.append(mem)
+    return mems
+
+
+def clock_hrs(clock:str) -> float:
+    hrs = 0
+    if '-' in clock:
+        days, clock = clock.split("-")
+        hrs += 24*float(days)
+    h,m,s = clock.split(":")
+    hrs += float(h)
+    hrs += float(m)/60
+    hrs += float(s)/3600
+    return hrs
+
+
+def get_times(infos:dict, unit='hrs') -> list:
+    times = []
+    for key, info in infos.items():
+        if 'running' in info[3].lower() or 'pending' in info[3].lower():
+            continue
+        clock = info[-3].split()[-1]
+        hrs = clock_hrs(clock)
+        times.append(hrs)
+    return times
+
+def sbatch(files):
+    if not type(files) == list:
+        files = [files]
+    for f in files:
+        os.chdir(op.dirname(f))
+        os.system('sbatch %s' % f)
+
+def getpids():
+    pids = os.popen('squeue -u lindb -o "%i"').read().split("
+")
+    pids = [p for p in pids if not p == '']
+    if len(pids) != luni(pids):
+        print('len !- luni pids')
+    return pids[1:]
+
+
+def getjobs(user='lindb'):
+    jobs = os.popen(f'squeue -u {user} -o "%j"').read().split("
+")
+    jobs = [j for j in jobs if not j == '']
+    if len(jobs) != luni(jobs):
+        print('len != luni jobs')
+    return jobs[1:]
+
+
+def getaccounts(pd=False, user='lindb'):
+    if pd == False:
+        accounts = os.popen(f'squeue -u {user} -o "%a"').read().split("
+")
+    else:
+        accounts = os.popen(f'squeue -u {user} -t "pd" -o "%a"').read().split('
+')
+    accounts = [a for a in accounts if not a in ['', 'ACCOUNT']]
+    return accounts
+
+
+def seff(pid):
+    lst = os.popen('seff %s' % pid).read().split("
+")
+    lst.remove('')
+    return lst
+
+
+def getpid(out):
+    return out.split("_")[-1].replace('.out', '')
+
+
+def get_mems(infos:dict, unit='MB') -> list:
+    mems = []
+    for key,info in infos.items():
+        if 'running' in info[3].lower() or 'pending' in info[3].lower():
+            continue
+        mem, units = info[-2].split()[-2:]
+        if units == 'GB':
+            mem = float(mem)*1024
+        elif units == 'EB':
+            pass
+        else:
+            try:
+                assert units == 'MB'
+            except AssertionError:
+                print('info = ', info)
+            mem = float(mem)
+        if unit == 'GB':
+            mem = mem/1024
+        mems.append(mem)
+    return mems
+
+
+def clock_hrs(clock:str) -> float:
+    hrs = 0
+    if '-' in clock:
+        days, clock = clock.split("-")
+        hrs += 24*float(days)
+    h,m,s = clock.split(":")
+    hrs += float(h)
+    hrs += float(m)/60
+    hrs += float(s)/3600
+    return hrs
+
+
+def get_times(infos:dict, unit='hrs') -> list:
+    times = []
+    for key, info in infos.items():
+        if 'running' in info[3].lower() or 'pending' in info[3].lower():
+            continue
+        clock = info[-3].split()[-1]
+        hrs = clock_hrs(clock)
+        times.append(hrs)
+    return times
---
+++ pythonimports.py
@@ -0,0 +1,147 @@
+import sys
+import os
+import pickle
+import random
+import string
+import math
+import shutil
+import time
+import numpy as np
+import pandas as pd
+from myslurm import *
+from tqdm import tqdm_notebook as nb
+from IPython.display import clear_output
+from collections import OrderedDict, Counter
+from IPython.display import Markdown, display
+from os import path as op
+from os import chdir as cd
+from os import getcwd as cwd
+from os import listdir
+from shutil import copy as cp
+from shutil import move as mv
+from ipyparallel import Client
+from decimal import Decimal
+from datetime import timedelta
+from datetime import datetime as dt
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+from matplotlib.backends.backend_pdf import PdfPages
+from matplotlib import pyplot as pl
+import matplotlib.dates as mdates
+
+pd.set_option('display.max_columns', 100)
+def ls(DIR):
+    return sorted([f for f in listdir(DIR)])
+def fs(DIR, dirs=None):
+    if dirs == False:
+        return sorted([f for f in fs(DIR) if not op.isdir(f)])
+    elif dirs == True:
+        return sorted([d for d in fs(DIR) if op.isdir(d)])
+    elif dirs == None:
+        return sorted([op.join(DIR, f) for f in ls(DIR)])
+def uni(mylist):
+    mylist = list(mylist)
+    return list(set(mylist))
+def luni(mylist):
+    return len(uni(mylist))
+def suni(mylist):
+    return sorted(uni(mylist))
+def nrow(df):
+    return len(df.index)
+def ncol(df):
+    return len(df.columns)
+def table(lst, exclude=[]):
+    c = Counter()
+    for x in lst:
+        c[x] += 1
+    if len(exclude) > 0:
+        for ex in exclude:
+            if ex in c:
+                c.pop(ex)
+    return c
+def pkldump(obj, f):
+    with open(f, 'wb') as o:
+        pickle.dump(obj, o, protocol=pickle.HIGHEST_PROTOCOL)
+def head(df):
+    return df.head()
+def update(args):
+    clear_output(wait=True)
+    [print(x) for x in args]
+def keys(Dict):
+    return list(Dict.keys())
+def values(Dict):
+    return list(Dict.values())
+def setindex(df, colname):
+    df.index = df[colname].tolist()
+    df.index.names = ['']
+    df = df[[c for c in df.columns if not colname == c]]
+    return df
+def pklload(path):
+    pkl = pickle.load(open(path, 'rb'))
+    return pkl
+def gettimestamp(f):
+    return time.ctime(os.path.getmtime(f))
+def getmostrecent(files, remove=False):
+    if not isinstance(files, list):
+        files = [files]
+    if len(files) > 1:
+        whichout = files[0]
+        dt1 = dt.strptime(gettimestamp(whichout)[4:], "%b %d %H:%M:%S %Y")
+        for o in files[1:]:
+            dt2 = dt.strptime(gettimestamp(o)[4:], "%b %d %H:%M:%S %Y")
+            if dt2 > dt1:
+                whichout = o
+                dt1 = dt2
+        if remove == True:
+            [os.remove(f) for f in files if not f == whichout]
+        return whichout
+    elif len(files) == 1:
+        return files[0]
+    else:
+        return None
+def formatclock(hrs):
+    # format the time
+    TIME = dt(1, 1, 1) + timedelta(hours=hrs)
+    # zero out the minutes, add an hour
+    if TIME.minute > 0:
+        TIME = TIME + timedelta(hours=1) - timedelta(minutes=TIME.minute)
+    # round up to 7 days, zero out the hours
+    if 3 < (TIME.day-1) < 7 or (3 <= TIME.day-1 and TIME.hour > 0):
+        diff = 7 - (TIME.day-1)
+        TIME = TIME + timedelta(days=diff) - timedelta(hours=TIME.hour)
+    # round up to 3 days, zero out the hours
+    if 1 < (TIME.day-1) < 3 or (1 <= TIME.day-1 and TIME.hour > 0):
+        diff = 3 - (TIME.day-1)
+        TIME = TIME + timedelta(days=diff) - timedelta(hours=TIME.hour)
+    # round up to 24 hrs, zero out the hours
+    if TIME.day == 1 and 12 < TIME.hour < 24:
+        TIME = TIME + timedelta(days=1) - timedelta(hours=TIME.hour)
+    # round up to 12 hrs 
+    if TIME.day == 1 and 3 < TIME.hour < 12:
+        diff = 12 - TIME.hour
+        TIME = TIME + timedelta(hours=diff)
+    # round up to 3 hrs 
+    if TIME.day == 1 and TIME.hour < 3:
+        diff = 3 - TIME.hour
+        TIME = TIME + timedelta(hours=diff) 
+    clock = "%s-%s:00:00"  % (TIME.day -1, str(TIME.hour).zfill(2))
+    return clock
+def printmd(string):
+    string = str(string)
+    display(Markdown(string))
+def makedir(directory):
+    if not op.exists(directory):
+        os.makedirs(directory)
+    return directory
+def getdirs(paths):
+    if isinstance(paths, str):
+        print('converting to list')
+        paths = [paths]
+    newdirs = []
+    for path in paths:
+        if op.isdir(path):
+            print(path)
+            newdirs.append(path)
+            newestdirs = getdirs(fs(path, dirs=True))
+            newdirs.extend(newestdirs)
+    return newdirs
